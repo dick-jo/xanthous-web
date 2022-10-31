8d21f203fa9202adedca537d8ebfd350664bd1e3
dick-jo <jones.rrw@gmail.com>
2022-10-31 21:07:36 +1030

    init

---
+++ style-dictionary.build.js
@@ -0,0 +1,8 @@
+const StyleDictionary = require('style-dictionary').extend('./style-dictionary.config.json')
+
+console.log('\n==============================================')
+
+StyleDictionary.buildAllPlatforms()
+
+console.log('\n==============================================')
+console.log('')
